1e2130357bfd1dbf921a172ca2d8d5070bc73cf1
Monika <412.kri@gmail.com>
2023-07-05 18:09:09 +0300

    config

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { server } from "./lib/server.js";
+
+console.clear();
+
+type App = {
+    init: () => void;
+}
+
+const app = {} as App;
+
+app.init = () => {
+    server.init()
+}
+
+app.init();
+
+export default app;
